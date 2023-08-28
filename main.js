7740aac903273f76d75550e3ebaa2cab88e75d32
Florian PASTEUR <florian.c.p@pm.me>
2023-08-28 14:36:53 +0200

    create template

---
+++ main.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           Rise to markdown
+// @description    Download a rise lesson to markdown
+// @author         Florian PASTEUR https://github.com/florianpasteur
+// @version        0.0.1
+// @icon           http://tampermonkey.net/favicon.ico
+// @match          <$URL$>
+// @grant          none
+// @run-at         document-end
+// ==/UserScript==
+
+/* jshint asi: true, esnext: true, -W097 */
+
+(function($) {
+    'use strict'
+
+    // Code...
+
+    console.log("Hello world")
+
+}).bind(this)();
